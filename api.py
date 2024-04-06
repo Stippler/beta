f215c381b106ffa88d6e77cf39f9423d2714e922
Christian Stippel <christian.stippel@tuwien.ac.at>
2024-04-06 08:59:48 +0200

    initial commit

---
+++ api.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+app = FastAPI()
+
+# Set up CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
+
+@app.get("/")
+async def read_root():
+    return {"message": "Hello World"}
+
+# You can add more endpoints as needed
